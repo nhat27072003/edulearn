9086c41e1f98559236afee2b5135ca4b1b02df45
nhat <nhat27072003@gmail.com>
2025-01-14 16:21:05 +0700

    initial project

---
+++ backend/src/main/java/com/ithust/edulearn/EdulearnApplication.java
@@ -0,0 +1,13 @@
+package com.ithust.edulearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdulearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdulearnApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ithust/edulearn/EdulearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ithust.edulearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdulearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
